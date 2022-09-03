8c588a42296a251065a9854e67d6b52941ce053f
EngineersBox <toastcraft.info@gmail.com>
2022-09-03 19:12:03 +1000

    Added macro support for print and println

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER
-        .lock()
-        .write_str("Some test thingy")
-        .unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", yeah these are numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
