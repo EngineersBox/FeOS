41977565728e5119dbd48a316df8b83704b3766b
u6940136 <u6940136@anu.edu.au>
2023-05-03 23:16:47 +1000

    Implemented boot info frame allocator

--- src/memory.rs
+++ src/memory.rs
@@ -11,6 +11,7 @@ use x86_64::{
     VirtAddr,
     PhysAddr,
 };
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
 /// Translates the given virtual address to the mapped physical address, or
 /// `None` if the address is not mapped.
@@ -110,6 +111,51 @@ unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
 
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
         None
+    }
+
+}
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+
+    /// Create a FrameAllocator from the passed memory map.
+    ///
+    /// This function is unsafe because the caller must guarantee that the passed
+    /// memory map is valid. The main requirement is that all frames that are marked
+    /// as `USABLE` in it are really unused.
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // Get usable regions from memory map
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        // Map each region to its address range
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        // Transform to an iterator of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // Create `PhysFrame` types from the start addresses
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        // No named existentially types supported in rust, so we can't store the iterator as a
+        // struct field
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
     }
 
 }
