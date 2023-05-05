6021b69498647260a51f7462d5a65d8e53c093ac
u6940136 <u6940136@anu.edu.au>
2023-05-05 23:42:27 +1000

    Implemented fixed size block allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,5 +1,6 @@
 pub mod bump;
 pub mod linked_list;
+pub mod fixed_size_block;
 
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
@@ -17,13 +18,15 @@ use x86_64::{
 use linked_list_allocator::LockedHeap;
 use bump::BumpAllocator;
 use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
 // static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+// static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 // static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub fn init_heap(mapper: &mut impl Mapper<Size4KiB>,
